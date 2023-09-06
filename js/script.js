2e662b5278061cfa6adef0cf09497d06cfff7f21
IreneChicca <irene.chicca@outlook.com>
2023-09-06 14:19:54 +0200

    Add mail list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+const { createApp } = Vue;
 
+createApp({
+  data() {
+    return {
+      mail: "",
+      prova: "sss",
+      arrayMail: [],
+    };
+  },
+
+  mounted() {
+    for (i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.mail = response.data.response;
+          this.arrayMail.push(response.data.response);
+        });
+    }
+  },
+}).mount("#app");
